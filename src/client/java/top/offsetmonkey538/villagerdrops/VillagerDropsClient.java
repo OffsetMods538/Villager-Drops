a04666e187ab07c3d6318afde4bd69b6d52a3932
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-11-10 11:14:19 +0200

    Initial Commit

---
+++ src/client/java/top/offsetmonkey538/villagerdrops/VillagerDropsClient.java
@@ -0,0 +1,11 @@
+package top.offsetmonkey538.villagerdrops;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VillagerDropsClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		// Do stuff
+	}
+}
