567039fb78ec25f994eb820418870bf869e4d28e
Quackster <quacksterr.15@gmail.com>
2025-07-30 17:48:18 +1000

    Upgrade to 1.21.8

--- src/main/java/io/github/offsetmonkey538/villager_drops/mixin/VillagerEntityMixin.java
+++ src/main/java/io/github/offsetmonkey538/villager_drops/mixin/VillagerEntityMixin.java
@@ -1,14 +1,20 @@
 package io.github.offsetmonkey538.villager_drops.mixin;
 
+import net.fabricmc.api.Environment;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradedItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
 
 @Mixin(VillagerEntity.class)
 public class VillagerEntityMixin {
@@ -16,18 +22,27 @@ public class VillagerEntityMixin {
     protected void afterUsing(TradeOffer offer, CallbackInfo ci) {
         SimpleInventory inventory = ((VillagerEntity)(Object) this).getInventory();
 
-        ItemStack firstBuyItem = offer.getAdjustedFirstBuyItem().copy();
-        ItemStack secondBuyItem = offer.getSecondBuyItem().copy();
+        TradedItem firstBuyTradedItem = offer.getFirstBuyItem();
+        Optional<TradedItem> secondBuyTradedItem = offer.getSecondBuyItem();
 
-        firstBuyItem.setCount(firstBuyItem.getCount() / 2);
-        secondBuyItem.setCount(secondBuyItem.getCount() / 2);
+        ItemStack firstBuyItem = null;
 
-        if (inventory.canInsert(firstBuyItem)) {
-            inventory.addStack(firstBuyItem);
+        if (firstBuyItem != null) {
+            firstBuyItem.setCount(firstBuyItem.getCount() / 2);
+
+            if (inventory.canInsert(firstBuyItem)) {
+                inventory.addStack(firstBuyItem);
+            }
         }
 
-        if (inventory.canInsert(secondBuyItem)) {
-            inventory.addStack(secondBuyItem);
+        if (secondBuyTradedItem.isPresent()) {
+            ItemStack secondBuyItem = secondBuyTradedItem.orElse(null).itemStack();
+
+            secondBuyItem.setCount(secondBuyItem.getCount() / 2);
+
+            if (inventory.canInsert(secondBuyItem)) {
+                inventory.addStack(secondBuyItem);
+            }
         }
     }
 
@@ -35,8 +50,11 @@ public class VillagerEntityMixin {
     public void onDeath(DamageSource damageSource, CallbackInfo ci) {
         VillagerEntity villager = (VillagerEntity) (Object) this;
 
+        if (villager.getWorld().isClient())
+            return;
+
         for (ItemStack stack : villager.getInventory().clearToList()) {
-            villager.dropStack(stack);
+            villager.dropStack((ServerWorld) villager.getWorld(), stack);
         }
     }
 }
