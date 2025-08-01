95d6e1fc1528137dda0808e4c34ee1c72dddeb0b
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-08-01 15:00:27 +0200

    Implement

---
+++ src/main/java/top/offsetmonkey538/villagerdrops/mixin/VillagerEntityMixin.java
@@ -0,0 +1,49 @@
+package top.offsetmonkey538.villagerdrops.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(VillagerEntity.class)
+public abstract class VillagerEntityMixin extends MerchantEntity {
+    public VillagerEntityMixin(EntityType<? extends MerchantEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(
+            method = "onDeath",
+            at = @At("TAIL")
+    )
+    private void villagerdrops$dropInventoryStacksOnDeath(DamageSource damageSource, CallbackInfo ci) {
+        this.getInventory().clearToList().forEach(this::dropStack);
+    }
+
+    @Inject(
+            method = "afterUsing",
+            at = @At("TAIL")
+    )
+    private void villagerdrops$addHalfTradeItemsToInventory(TradeOffer offer, CallbackInfo ci) {
+        villagerdrops$handleAddingItemStackToInventory(offer.getAdjustedFirstBuyItem());
+        villagerdrops$handleAddingItemStackToInventory(offer.getSecondBuyItem());
+    }
+
+    @Unique
+    private void villagerdrops$handleAddingItemStackToInventory(ItemStack stack) {
+        final SimpleInventory inventory = this.getInventory();
+        stack = stack.copy();
+
+        stack.setCount(stack.getCount() / 2);
+
+        if (inventory.canInsert(stack)) inventory.addStack(stack);
+    }
+}
